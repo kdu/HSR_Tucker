e917f0d9a643a27ed4455ab42b099fa824451ed0
Clémence <clemence.prevost@univ-lorraine.fr>
2018-12-12 08:52:16 +0100

    no message

--- demos/test_simulated.m
+++ demos/test_simulated.m
@@ -67,7 +67,6 @@ d1 = 4; d2 = 4; q = 9;
 [P1,P2] = spatial_deg(X, q, d1, d2);
 HSI_true = tmprod(tmprod(X,P1,1),P2,2);
 
-
 %% 3rd EXAMPLE ON SIMULATED DATA
 
 I = 240; J = 240; %spatial dimensions
@@ -107,21 +106,20 @@ d1 = 4; d2 = 4; q = 9;
 [P1,P2] = spatial_deg(X, q, d1, d2);
 HSI_true = tmprod(tmprod(X,P1,1),P2,2);
 
-
 %% 4rd EXAMPLE ON SIMULATED DATA
 
-I = 360; J = 360; %spatial dimensions
+I = 240; J = 240; %spatial dimensions
 A1 = zeros(I,J); A2 = A1; A3 = A1; A4 = A1; 
 A5 = A1; A6 = A1; %initialize abundance maps
 
-g1 = fspecial('gaussian',60, 20);
-A1(1:60,1:60) = g1; 
-A2(61:120,1:60) = g1; A2(1:60,61:120) = g1;
-A3(121:180,1:60) = g1; A3(61:120,61:120) = g1; A3(1:60,121:180) = g1; 
-A4(181:240,1:60) = g1; A4(1:60,181:240) = g1; 
-A5(241:300,1:60) = g1; A5(181:240,61:120) = g1; A5(61:120,181:240) = g1; A5(1:60,241:300) = g1;
-A6(301:360,1:60) = g1; A6(241:300,61:120) = g1; A6(181:240, 121:180) = g1;
-    A6(121:180,181:240) = g1; A6(61:120,241:300) = g1; A6(1:60,301:360) = g1;
+g1 = fspecial('gaussian',40, 15);
+A1(1:40,1:40) = g1; 
+A2(41:80,1:40) = g1; A2(1:40,41:80) = g1;
+A3(81:120,1:40) = g1; A3(41:80,41:80) = g1; A3(1:40,81:120) = g1; 
+A4(121:160,1:40) = g1; A4(1:40,121:160) = g1; 
+A5(161:200,1:40) = g1; A5(121:160,41:80) = g1; A5(41:80,121:160) = g1; A5(1:40,161:200) = g1;
+A6(201:240,1:40) = g1; A6(161:200,41:80) = g1; A6(121:160, 81:120) = g1;
+    A6(81:120,121:160) = g1; A6(41:80,161:200) = g1; A6(1:40,201:240) = g1;
     
 
 K = 200; %spectral dimension
@@ -147,31 +145,26 @@ s6 = real(mean(mat(:,ind),2));
 clear SRI; clear mat; clear ind;
 
 X = outprod(A1,s1) + outprod(A2,s2) + outprod(A3,s3) + outprod(A4,s4) + outprod(A5,s5) + outprod(A6,s6);
-% Pm = zeros(2,size(X,3));
-% Pm(1,1:100) = (1/100)*ones(1,100); Pm(2,101:200) = (1/100)*ones(1,100);
-% Pm = zeros(4,size(X,3));
-% Pm(1,1:50) = (1/50)*ones(1,50); Pm(2,51:100) = (1/50)*ones(1,50);
-% Pm(3,101:150) = (1/50)*ones(1,50); Pm(4,151:200) = (1/50)*ones(1,50);
-Pm = spectral_deg(X,"LANDSAT");
+Pm = spectral_deg(X,"Quickbird");
+%Pm = spectral_deg(X,"LANDSAT");
 MSI_true = tmprod(X,Pm,3);
 d1 = 4; d2 = 4; q = 9;
 [P1,P2] = spatial_deg(X, q, d1, d2);
 HSI_true = tmprod(tmprod(X,P1,1),P2,2);
 
 
-%     for k=1:size(MSI_true,3)
-%         MSI(:,:,k) = awgn(MSI_true(:,:,k),Inf);
-%     end
-%     for k=1:size(HSI_true,3)
-%         HSI(:,:,k) = awgn(HSI_true(:,:,k),Inf);
-%     end
-
+    for k=1:4
+        MSI(:,:,k) = awgn(MSI_true(:,:,k),20);
+    end
+    for k=1:size(HSI_true,3)
+        HSI(:,:,k) = awgn(HSI_true(:,:,k),20);
+    end
 
 %% RUN SCOTT AUTOMATICALLY
 
 lvl = ["20","35","60"];
 %param = [2,6; 3,2; 4,2; 6,4];
-param = [3,2; 4,2; 6,4];
+param = [6,4; 6,6];
 Niter = 10; lambda = 1; 
 
 for i=1:4
@@ -179,30 +172,35 @@ for i=1:4
         snr_stereo = []; snr_scott = [];
         eval(sprintf('load(''data_%dS%dB%sdB.mat'')',param(i,1),param(i,2),lvl(j)))
         
-        for F=1:50
+        for F=1:25
             F
             [SRI_hat, ~] = stereo3(HSI, MSI, P1,P2,Pm, F);
             SRI_hat = real(SRI_hat); snr_stereo(F,1) = r_snr(X,SRI_hat);
         end
         
+        
         figure
-        plot(1:50,snr_stereo)
+        plot(1:25,snr_stereo)
         xlabel('F'); ylabel('SNR (dB)')
-        title(sprintf('STEREO, %d sources, Km=%d, %sdB noise',param(i,1),param(i,2),lvl(j)))
+        title(sprintf('STEREO, %d sources, Km=%d, %sdB SNR',param(i,1),param(i,2),lvl(j)))
         savefig(gcf, sprintf('SNR_CP%dM%sdB',param(i,1),lvl(j)))
     
-        for r1=1:size(HSI,1)
+        for r1=1:size(HSI,1)+10
             for r3=1:10
                 R = [r1,r1,r3]
                 [SRI_hat,~] = scott(HSI, MSI, P1, P2, Pm, R);
                 snr_scott(r1,r3) = r_snr(X,SRI_hat);
+                
+                if (r3>size(MSI,3) && r1>size(HSI,1))%identifiability
+                     snr_scott(r1,r3) = NaN;
+                end
             end
         end
         
         figure
-        surfc(1:10,1:79,snr_scott)
+        surfc(1:10,1:size(HSI,1)+10,snr_scott)
         xlabel('R_3'); ylabel('R_1 = R_2'); zlabel('SNR (dB)')
-        title(sprintf('SCOTT, %d sources, Km=%d, %sdB noise',param(i,1),param(i,2),lvl(j)))
+        title(sprintf('SCOTT, %d sources, Km=%d, %sdB SNR',param(i,1),param(i,2),lvl(j)))
         savefig(gcf, sprintf('SNR_SVD%dM%sdB',param(i,1),lvl(j)))
         
            
@@ -219,7 +217,7 @@ for i=1:4
     eval(sprintf('load(''data_%dS%dBInfdB.mat'')',param(i,1),param(i,2)))
     
 
-        for F=1:50
+        for F=1:25
             F
             try
                 [SRI_hat, ~] = stereo3(HSI, MSI, P1,P2,Pm, F);
@@ -231,12 +229,12 @@ for i=1:4
         end
         
         figure
-        plot(1:50,snr_stereo)
+        plot(1:25,snr_stereo)
         xlabel('F'); ylabel('SNR (dB)'); xlim([1 50])
-        title(sprintf('STEREO, %d sources, Km=%d, Inf dB noise',param(i,1),param(i,2)))
+        title(sprintf('STEREO, %d sources, Km=%d, Inf dB SNR',param(i,1),param(i,2)))
         savefig(gcf, sprintf('SNR_CP%dMInfdB',param(i,1)))
         
-        for r1=1:size(HSI,1)
+        for r1=1:size(HSI,1)+10
             for r3=1:10
                 R = [r1,r1,r3]
                 try
@@ -246,13 +244,17 @@ for i=1:4
                     snr_scott(r1,r3) = NaN;
                     continue
                 end
+                
+                if (r3>size(MSI,3) && r1>size(HSI,1))%identifiability
+                     snr_scott(r1,r3) = NaN;
+                end
             end
         end
         
         figure
         surfc(1:10,1:size(HSI),snr_scott)
         xlabel('R_3'); ylabel('R_1 = R_2'); zlabel('SNR (dB)')
-        title(sprintf('SCOTT, %d sources, Km=%d, Inf dB noise',param(i,1),param(i,2)))
+        title(sprintf('SCOTT, %d sources, Km=%d, Inf dB SNR',param(i,1),param(i,2)))
         savefig(gcf, sprintf('SNR_SVD%dMInfdB',param(i,1)))
 
 
