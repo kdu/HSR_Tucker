b302269d17b39f04d28eaa10f401f53e1b000ea8
Clémence <clemence.prevost@univ-lorraine.fr>
2019-01-24 11:27:56 +0100

    Finished test_indianpines_spectra

--- demos/test_indianpines_spectra.m
+++ demos/test_indianpines_spectra.m
@@ -18,8 +18,73 @@ load('Indian_pines_gt.mat'); indian_pines_gt(:,1) = []; indian_pines_gt(1,:) = [
 
 %% Figure - original spectral for mat. 4,7,9,14
 
-
+X = tens2mat(SRI,3,[]); mat = [4 7 9 14];
+figure
+for i=1:length(mat)
+    ind = find(reshape(indian_pines_gt,1,[]) == mat(i));
+    s = real(mean(X(:,ind),2));
+    
+    plot(s); hold on
+end
+title('Original spectra'), legend('Mat.4','Mat.7','Mat.9','Mat.14')
 
 %% Figure - residual error
 
+methods = {'STEREO' 'stereo3' '16'; ...
+           'SCOTT' 'scott' '[40,40,6]';...
+           'SCOTT' 'scott' '[30,30,16]';...
+           'SCOTT' 'scott' '[24,24,25]';};      
+DegMat = struct('Pm', Pm, 'P1', P1, 'P2', P2);         
+[~, est] = compare_methods(SRI, HSI, MSI, DegMat, [d1 d2], methods); 
+
+X = tens2mat(SRI,3,[]); mat = [4 7 9 14];
+Xhat1 = tens2mat(real(est{1}),3,[]); Xhat2 = tens2mat(real(est{2}),3,[]);
+Xhat3 = tens2mat(real(est{3}),3,[]); Xhat4 = tens2mat(real(est{4}),3,[]);
+
+figure
+for i=1:length(mat)
+    ind = find(reshape(indian_pines_gt,1,[]) == mat(i));
+    s = real(mean(X(:,ind),2)); s1 = real(mean(Xhat1(:,ind),2));
+    s2 = real(mean(Xhat2(:,ind),2)); s3 = real(mean(Xhat3(:,ind),2));
+    s4 = real(mean(Xhat4(:,ind),2));
+    
+    subplot(2,2,i)
+    plot((s-s1)./s);hold on;plot((s-s2)./s);hold on;plot((s-s3)./s);hold on;plot((s-s4)./s)
+     title(sprintf('Material %d',mat(i)))
+    legend(sprintf('%s,%s',methods{1,1},methods{1,3}),...
+        sprintf('%s,%s',methods{2,1},methods{2,3}),...
+        sprintf('%s,%s',methods{3,1},methods{3,3}),...
+        sprintf('%s,%s',methods{4,1},methods{4,3}));
+end
+
 %% Figure - spectral bins 80 to 100
+
+methods = {'STEREO' 'stereo3' '50'; ...
+           'SCOTT' 'scott' '[40,40,6]';...
+           'SCOTT' 'scott' '[30,30,16]';...
+           'SCOTT' 'scott' '[24,24,25]';};      
+DegMat = struct('Pm', Pm, 'P1', P1, 'P2', P2);         
+[~, est] = compare_methods(SRI, HSI, MSI, DegMat, [d1 d2], methods); 
+
+X = tens2mat(SRI,3,[]); mat = [4 7 9 14];
+Xhat1 = tens2mat(real(est{1}),3,[]); Xhat2 = tens2mat(real(est{2}),3,[]);
+Xhat3 = tens2mat(real(est{3}),3,[]); Xhat4 = tens2mat(real(est{4}),3,[]);
+
+figure
+for i=1:length(mat)
+    ind = find(reshape(indian_pines_gt,1,[]) == mat(i));
+    s = real(mean(X(:,ind),2)); s1 = real(mean(Xhat1(:,ind),2));
+    s2 = real(mean(Xhat2(:,ind),2)); s3 = real(mean(Xhat3(:,ind),2));
+    s4 = real(mean(Xhat4(:,ind),2));
+    
+    subplot(2,2,i)
+    plot(s(80:100));hold on; plot(s1(80:100));hold on; plot(s2(80:100));...
+        hold on; plot(s3(80:100)); hold on;  plot(s4(80:100))
+     title(sprintf('Material %d',mat(i)))
+    legend('Original spectrum',...
+        sprintf('%s,%s',methods{1,1},methods{1,3}),...
+        sprintf('%s,%s',methods{2,1},methods{2,3}),...
+        sprintf('%s,%s',methods{3,1},methods{3,3}),...
+        sprintf('%s,%s',methods{4,1},methods{4,3}));
+end
+
